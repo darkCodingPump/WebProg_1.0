64e88d1949a2dd6c25af23e681db60803ba47edc
Tobias Witte <tobias.witte@capgemini.com>
2022-11-11 09:49:28 +0100

    *big-bababubu-int

--- htdocs/migrations/m0001_initial.php
+++ htdocs/migrations/m0001_initial.php
@@ -4,7 +4,7 @@ class m0001_initial{
 
     public function up(){
         $db = \app\core\Application::$app->db;
-        $SQL = "CREATE TABLE users (id INT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(512) NOT NULL);";
+        $SQL = "CREATE TABLE users (id BIGINT AUTO_INCREMENT PRIMARY KEY, email VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(512) NOT NULL);";
         $db->pdo->exec($SQL);
     }
 
